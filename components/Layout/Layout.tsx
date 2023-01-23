661fce9cfff39cb80c5c62ff216bb957c19a908e
Michal Kozlowski <michalkozlowski@Michals-MacBook-Air.local>
2023-01-23 17:19:39 +0100

    poprawka fontu

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -7,6 +7,7 @@ import { FooterType } from '@typings/footer';
 
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin-ext', 'latin'],
 });
 
 type Props = {
