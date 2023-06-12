fb19742bb03eeeb6c5df0e5eff57d42ebebc301d
Michal Kozlowski <michalkozlowski@Michals-MacBook-Air.local>
2023-06-12 13:26:50 +0200

    dodatnie gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,13 @@ import '../styles/globals.scss';
 import '../styles/reset.css';
 import type { AppProps } from 'next/app';
 import Layout from '@components/Layout/Layout';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-D9BVJ5FKQB' });
+  }, []);
   return (
     <Layout pages={pageProps.pages} footer={pageProps.footer}>
       <Component {...pageProps} />
