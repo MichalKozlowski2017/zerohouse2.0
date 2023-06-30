c38e140e1cf3f575393108a793f7e91bc538a3af
MichalKozlowski2017 <m.kozlowski87@gmail.com>
2023-06-30 11:49:46 +0200

    zmiana ga zerohouse

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,18 @@ import type { AppProps } from 'next/app';
 import Layout from '@components/Layout/Layout';
 import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-WG2VLX3' });
   }, []);
   return (
-    <Layout pages={pageProps.pages} footer={pageProps.footer}>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Layout pages={pageProps.pages} footer={pageProps.footer}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
